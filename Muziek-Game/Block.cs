0fbf8f06af9701699ed4491b8891dcb79f753a9c
Harm Haandrikman <hfhaandrikman@gmail.com>
2024-09-23 13:52:13 +0200

    portaal is gespawned en blokken basis cs

---
+++ Muziek-Game/Block.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Muziek_Game
+{
+    internal class Block
+    {
+        private Rectangle block_obj;
+        private double Speed = 1;
+        private Canvas GameCanvas;
+
+        public Block(Canvas canvas, int starty, int startx, bool firstrow)
+        {
+            int width = 50;
+            int height = 50;
+
+            GameCanvas = canvas;
+
+            block_obj = new Rectangle()
+            {
+                Width = width,
+                Height = height,
+                Fill = System.Windows.Media.Brushes.Red
+            };
+
+            Canvas.SetLeft(block_obj, startx);
+            if (firstrow)
+                Canvas.SetTop(block_obj, starty);
+            else
+            Canvas.SetTop(block_obj, starty - 50);
+
+            GameCanvas.Children.Add(block_obj);
+        }
+
+
+    }
+}
--- Muziek-Game/MainWindow.xaml.cs
+++ Muziek-Game/MainWindow.xaml.cs
@@ -20,9 +20,98 @@ namespace Muziek_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //pubieke variablen
+        public int[] portaalpositie = { 1000, 200 };
+
+
         public MainWindow()
         {
             InitializeComponent();
+            Canvas GameCanvas = new Canvas();
+            StartGame();
+        }
+
+        public int StartGame()
+        {
+            try
+            {
+                // Start de game zo mogelijk
+
+                initializePlayer("Player-1", "Default", 250);
+                initializePortal();
+
+                return 1;
+
+            }
+            catch (Exception)
+            {
+                // Catch Error als er een is
+
+                //hier de rest van de codebase!
+
+                return 0;
+            }
+        }
+
+        public int initializePlayer(string name, string skin, int Health)
+        {
+            try
+            {
+                // Creeer een nieuwe speler
+
+                //hier de rest van de codebase!
+
+                return 1;
+
+            }
+            catch (Exception)
+            {
+                // Catch Error als er een is
+
+                //hier de rest van de codebase!
+
+                return 0;
+            }
+        }
+
+        private Rectangle portal;
+
+        public int initializePortal()
+        {
+
+            try
+            {
+                portal = new Rectangle
+                {
+                    Width = 250,
+                    Height = 300,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 4
+
+                };
+
+                ImageBrush imageBrush = new ImageBrush();
+                imageBrush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Assets/NetherPortal.png", UriKind.Absolute));
+                portal.Fill = imageBrush;
+
+                Canvas.SetLeft(portal, portaalpositie[0]); // Horizontaal (offset vanaf links)
+                Canvas.SetTop(portal, portaalpositie[1]);  // Verticaal (offset vanaf boven)
+
+                GameCanvas.Children.Add(portal);
+
+                return 1;
+
+            }
+            catch (Exception error)
+            {
+                // Catch Error als er een is
+
+                Console.WriteLine(error);
+
+                return 0;
+            }
+
         }
     }
 }
